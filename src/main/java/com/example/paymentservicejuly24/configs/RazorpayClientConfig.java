a0130b93e49ae732953b8defe49c1d682a682955
Preeti <preeti.kumari241116@gmail.com>
2025-04-04 07:29:10 +0530

    Adding some comments

--- src/main/java/com/example/paymentservicejuly24/configs/RazorpayClientConfig.java
+++ src/main/java/com/example/paymentservicejuly24/configs/RazorpayClientConfig.java
@@ -19,6 +19,8 @@ public class RazorpayClientConfig {
 
         System.out.println("Adding a random print statement to test git commands.");
 
+        System.out.println("adding some lines PK");
+
         return new RazorpayClient(razorpayKeyId, razorpayKeySecret);
     }
 }
